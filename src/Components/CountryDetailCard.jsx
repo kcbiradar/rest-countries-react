86666d2878de7ecec1f6e66a6da7907d603b8d37
Khajappa Biradar <khajappa.biradar.28.2@mountblue.tech>
2024-06-16 17:58:21 +0530

    country-details page is updated and added design.

--- src/Components/CountryDetailCard.jsx
+++ src/Components/CountryDetailCard.jsx
@@ -24,23 +24,52 @@ export default function CountryDetailCard() {
 
   const country = data.filter((each_country) => each_country.cca3 === location);
 
+  console.log(country[0]);
+
   return (
     <>
         <Navbar />
+        <div className='back-btn'>
         <Link to="/"><button>Back</button></Link>
+        </div>
         {country[0] ? <>
-                ({
+                {   
+                    
                     <div className='detail'>
                         <div className='countryDetailsImage'>
-                            <img src={country[0].flags.png} alt="country-image" />
+                            <img src={country[0].flags.png} alt="country-image" className='country-detail-image'/>
                         </div>
                         <div className='displayCountryDetails'>
-                            <div className='countryLeftDetails'>
-
+                          <div className='country-name'>
+                              <h2>{country[0].name.common}</h2>
+                          </div>
+                            <div className='inner-div'>
+                              <div className='left-section-detail'>
+                                  <div>
+                                    <p><b>Native Name: </b>{country[0].name.common}</p>
+                                    <p><b>Population: </b>{country[0].population}</p>
+                                    <p><b>Region: </b>{country[0].region}</p>
+                                    <p><b>Sub Region: </b>{country[0].subregion}</p>
+                                    <p><b>Capital: </b>{country[0].capital}</p>
+                                  </div>
+                              </div>
+                              <div>
+                                  <p><b>Top Level Domain: </b>{country[0].tld[0]}</p>
+                                  <p><b>Currencies: </b>{Object.keys(country[0].currencies).join(', ')}</p>
+                                  <p><b>Languages: </b>{Object.values(country[0].languages).join(', ')}</p>
+                              </div>
+                            </div>
+                            <div className='borders'>
+                              <div><b>Borders:</b></div>
+                              {
+                                country[0]?.borders.map((border,index) => {
+                                  return <Link key={index} to={`/countryDetails/${border}`}><button >{border}</button></Link>
+                                })
+                              }
                             </div>
                         </div>
                     </div>
-                })
+                }
         </> : <h1>Loading...</h1>}
     </>
   )
